fd992c9c7bf5d48c0b6a599f8978c6c6beeb009c
chenquan <chenquan.dev@gmail.com>
2022-07-28 20:24:08 +0800

    refactor code

---
+++ hash32_test.go
@@ -0,0 +1,56 @@
+//   Copyright 2022 chenquan
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package orderhash
+
+import (
+	"hash/crc32"
+	"strconv"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHash32(t *testing.T) {
+	f := Hash32(crc32.ChecksumIEEE)
+	N := 10000
+	group := sync.WaitGroup{}
+	m := sync.Map{}
+	for i := 0; i < N; i++ {
+		i := i
+		group.Add(1)
+		go func() {
+			defer group.Done()
+			code := f([]byte(strconv.Itoa(i)))
+			_, ok := m.Load(i)
+			assert.False(t, ok)
+			m.Store(code, i)
+		}()
+	}
+	group.Wait()
+
+	for i := 0; i < N; i++ {
+		i := i
+		group.Add(1)
+		go func() {
+			defer group.Done()
+			code := f([]byte(strconv.Itoa(i)))
+			v, ok := m.Load(code)
+			assert.True(t, ok)
+			assert.EqualValues(t, v, i)
+		}()
+	}
+	group.Wait()
+}
