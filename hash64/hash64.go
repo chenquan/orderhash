cfc248b1b06cd1cd7c850e429dcbfb06fc344a1a
chenquan <chenquan.dev@gmail.com>
2022-07-28 16:51:17 +0800

    init commit

---
+++ hash64/hash64.go
@@ -0,0 +1,45 @@
+//   Copyright 2022 chenquan
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package hash64
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+type Hash struct {
+	hashFunc func(b []byte) uint64
+	m        *sync.Map
+	n        uint64
+}
+
+func New(hashFunc func(b []byte) uint64) *Hash {
+	return &Hash{
+		hashFunc: hashFunc,
+		m:        &sync.Map{},
+	}
+}
+
+func (h *Hash) Sum(b []byte) (index uint64) {
+	hashCode := h.hashFunc(b)
+	indexVal, ok := h.m.Load(hashCode)
+	if !ok {
+		index = atomic.AddUint64(&h.n, 1) - 1
+		h.m.Store(hashCode, index)
+		return index
+	}
+
+	return indexVal.(uint64)
+}
